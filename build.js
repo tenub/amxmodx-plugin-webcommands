a4ef0591f2d383287e52f9693455556cbf5ae249
Joseph Chrzan <jchrzan1@gmail.com>
2016-05-04 23:55:47 -0400

    fix gamepath

--- build.js
+++ build.js
@@ -1,1 +1,1 @@
-require('amxmodx-build')({ gamepath: 'C:/Program Files (x86)/Steam/steamapps/common/Half-Life/cstrike/addons/amxmodx' });
+require('amxmodx-build')({ gamepath: 'C:/Program Files (x86)/Steam/steamapps/common/Half-Life/cstrike' });
